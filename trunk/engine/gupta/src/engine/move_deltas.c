19dbd130c0f694b4dfc28a79630d5f25c10d3491
bughunter2 <bughunter2@8f2dcbc6-0ef0-49a1-a563-a65aa5bd89ca>
2014-09-25 14:57:51 +0000

    add license headers to gupta source code

--- trunk/engine/gupta/src/engine/move_deltas.c
+++ trunk/engine/gupta/src/engine/move_deltas.c
@@ -1,3 +1,16 @@
+/*
+    Written by Jelle Geerts (jellegeerts@gmail.com).
+
+    To the extent possible under law, the author(s) have dedicated all
+    copyright and related and neighboring rights to this software to
+    the public domain worldwide. This software is distributed without
+    any warranty.
+
+    You should have received a copy of the CC0 Public Domain Dedication
+    along with this software.
+    If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+*/
+
 #include "move_deltas.h"
 
 static const s8 king_deltas[]   = {-0x01, +0x0F, +0x10, +0x11, +0x01, -0x0F, -0x10, -0x11, 0};
